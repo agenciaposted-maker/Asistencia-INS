33b1484b3fef55551265d8252b3f58e7d55f3a28
agenciaposted-maker <agenciaposted@gmail.com>
2026-03-20 11:18:25 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'asistencia-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.add('/Asistencia-INS/'))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res && res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => 
+        caches.match(e.request)
+          .then(r => r || caches.match('/Asistencia-INS/'))
+      )
+  );
+});
